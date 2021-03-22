55608e45e6fa50343df078a02fb79311bb91d69d
AmineDevF <amine.devinfo@gmail.com>
2021-03-23 00:16:54 +0100

    deploy

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -6,7 +6,7 @@ const Tasks = ({tasks , onDelete , onToggle}) => {
         return (
             <>
             {tasks.map((task)=>(
-                // <h3>{task.text}</h3>
+             
 
                 <Task 
                 key={task.id}
